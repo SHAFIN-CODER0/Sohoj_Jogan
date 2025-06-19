c1384b656db0827bf253abd957e408e42444348b
Najmul Hossain Nur <155439013+md-najmul-hossain-nur@users.noreply.github.com>
2025-06-19 23:09:52 +0600

    Delete java_script directory

--- java_script/Email.js
+++
@@ -1,9 +0,0 @@
-function openEmailModal() {
-    document.getElementById('emailModal').style.display = 'flex';
-  }
-  function openMail() {
-window.open('https://mail.google.com/mail/?view=cm&fs=1&to=sohojjogan@gmail.com&su=&body=', 'GmailCompose', 'width=800,height=600');
-}
-  function closeEmailModal() {
-    document.getElementById('emailModal').style.display = 'none';
-  }
